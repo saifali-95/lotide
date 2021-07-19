6603066b10cdd137f107f46afcafc8cfb70bb21f
Saif Ali <saifali7646@gmail.com>
2021-07-19 23:45:04 +0000

    Added head finding function

---
+++ head.js
@@ -0,0 +1,17 @@
+//FINDING HEAD OF THE PROVIDED INPUT ARRAY:
+const head = function(array_1){
+  return array_1[0];
+}
+
+// FUNCTION FOR TEST CASE:
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
