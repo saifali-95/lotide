e22f8782399d06159d5d9849356df741e579cd8c
Saif Ali <saifali7646@gmail.com>
2021-07-21 17:36:24 +0000

    Added isEven and isOdd function

--- middle.js
+++ middle.js
@@ -19,16 +19,27 @@ const eqArrays = function (array1, array2) {
   return true;
 };
 
+// Even Function 
+
+function isEven(n) {
+  return n % 2 == 0;
+}
+
+// ODD function 
+function isOdd(n) {
+  return Math.abs(n % 2) == 1;
+}
+
 //FUNCTION TO FIND THE MIDDLE ELEMENTS OF AN ARRAY.
 
 const middle = function (inputArray){
   if (inputArray.length <= 2){
     return [];
   }
-  if (Number.isInteger(inputArray.length/2)) {
+  if (isEven(inputArray.length)) {
     return [inputArray[(inputArray.length/2)-1],inputArray[inputArray.length/2]];
   }
-  if (!Number.isInteger(inputArray.length/2)){
+  if (isOdd(inputArray.length)){
     return [inputArray[(inputArray.length-1)/2]];
   }
 }
