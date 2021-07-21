0bf0907ad36719f0bcc11a229fde24717c7f7ffe
Saif Ali <saifali7646@gmail.com>
2021-07-21 00:33:55 +0000

    eqArrays function two compare two input arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION TO ASSERT PASS OR FAIL, IF INPUTS ARE SAME OR DIFFERENT.
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//OUTPUT FALSE OR TRUE BY COMPARING TWO INPUT ARRAYS
+
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//TEST CASE
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
