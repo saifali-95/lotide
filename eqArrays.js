309610a5a038bc452635d7ad569a97a28cb380ff
Saif Ali <saifali7646@gmail.com>
2021-07-22 12:11:53 +0000

    added condition to check length of both arrays

--- eqArrays.js
+++ eqArrays.js
@@ -11,6 +11,9 @@ const assertEqual = function (actual, expected) {
 //OUTPUT FALSE OR TRUE BY COMPARING TWO INPUT ARRAYS
 
 const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length){
+   return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false;
@@ -25,5 +28,5 @@ console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2"])) // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
