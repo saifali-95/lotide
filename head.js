cf1580560144fc013ff0d470cbe2855269bc2646
Saif Ali <saifali7646@gmail.com>
2021-07-26 17:19:18 +0000

    Refactor functions by exporting/required commands

--- head.js
+++ head.js
@@ -1,17 +1,9 @@
+const assertEqual = require('./assertEqual');
+
+
 //FINDING HEAD OF THE PROVIDED INPUT ARRAY:
 const head = function(array_1){
   return array_1[0];
 }
 
-// FUNCTION FOR TEST CASE:
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+module.exports = head;
