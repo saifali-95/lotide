832908696e497f17eea0960da3edcefb455d5c8b
Saif Ali <saifali7646@gmail.com>
2021-07-26 18:25:13 +0000

    refactor function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,5 @@
-//OUTPUT FALSE OR TRUE BY COMPARING TWO INPUT ARRAYS
-const eqArrays = function (array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+
 
 
 //PRINT PASSED OR FAILED IF TWO INPUT ARRAYS ARE SAME OR DIFFERENT RESPECTIVELY.
@@ -19,7 +12,4 @@ const assertArraysEqual = function (array1, array2){
   }
 }
 
-//TEST CASE:
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(['1', '2', '3'], [1, 2, 3]);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual')
+
+//TEST CASE:
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(['1', '2', '3'], [1, 2, 3]);
